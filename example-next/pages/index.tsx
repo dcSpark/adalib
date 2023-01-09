1c5cd8fa34de1a3b1b5646a6d4e698369ff5384d
Gabe <ggaabe@gmail.com>
2023-01-09 15:31:35 -0600

    package updates

--- example-next/pages/index.tsx
+++ example-next/pages/index.tsx
@@ -4,7 +4,6 @@ import {
   disconnect,
   getBalance,
   submitTx,
-  // getConnectorIsAvailable,
   FlintConnector,
   // watchAddress,
   // getFeeForMessage,
--- src/utils/connector.ts
+++ src/utils/connector.ts
@@ -4,7 +4,6 @@ import { getActiveConnector } from '../store';
 export async function withConnector<T>(withConnectorFunc: (connector: Connector) => Promise<T>) {
   const connector = getActiveConnector();
 
-  // TODO: Add is available check. isEnabled is insufficient because a connector can available but not enabled
   if (connector.isAvailable()) return withConnectorFunc(connector);
 
   return null;
