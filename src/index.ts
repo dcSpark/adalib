fc6e04fb7321ed18505f5051a1f28403384afa52
Gabe <ggaabe@gmail.com>
2023-01-23 23:57:50 -0600

    exports watchAddress

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ export * from './actions';
 export * from './defaults';
 export * from './connectors';
 export { cardano } from './types';
-export { getActiveConnector, getConnectorIsAvailable } from './store';
+export { getActiveConnector, getConnectorIsAvailable, watchAddress } from './store';
 polyfill();
 
 export type { Chain } from './types/chain';
